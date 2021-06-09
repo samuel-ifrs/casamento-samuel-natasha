15f7bc932e66d8fb9590a75574209ebae3916546
Samuel Andrade <samuel-ifrs>
2021-06-08 21:13:58 -0300

    teste

---
+++ index.php
@@ -0,0 +1,32 @@
+<HTML>
+  <HEAD>
+    <TITLE>Samuel ePHP Natasha</TITLE>
+    <link rel="StyleSheet" href="https://fonts.googleapis.com/css?family=Playfair+Display"></link>
+    <link rel="StyleSheet" href="https://fonts.googleapis.com/css?family=Tinos:400,700"></link>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <link rel="StyleSheet" href="estilo.css"></link>
+  </HEAD>
+  <BODY class="principal">
+    <div id="conteudos">
+      <div id="barraTitulo" class="parte linha">
+        <span id="cronometro">FALTAM <span id="cronometroDias">XX</span> DIAS</span>
+        <div id="titulo">
+          <span id="tituloE">&amp;</span>
+          <div id="nomes">
+            <span>Samuel</span>
+            <span>Natasha</span>
+          </div>        
+        </div>
+        <img src="imagens/hearts.png" />
+        <div id="dataCasamento">07.08.2021</div>
+      </div>
+      <div id="barraPaginas">
+        <a target="conteudo" href="pag_home.html" class="selecionado">HOME</a>
+        <a target="conteudo" href="pag_presentes.html">LISTA DE PRESENTES</a>
+        <a target="conteudo" href="pag_recados.html">RECADOS</a>
+      </div>
+      <div id="conteudo"></iframe>
+    </div>
+    <script src="script.js"></script>
+  </BODY>
+</HTML>
